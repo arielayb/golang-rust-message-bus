06bd1faf9a57e78c445aeb456e82c1c03178a587
Steam Deck User <deck@steamdeck.hsd1.co.comcast.net>
2025-05-06 22:49:04 -0600

    added graph system and prompt library

--- RustMQ/src/main.rs
+++ RustMQ/src/main.rs
@@ -1,7 +1,27 @@
 use std::net::UdpSocket;
+use inquire::{Confirm, Select, Text};
 
 mod rustymq;
 
 fn main() {
-    
+    // Example using a text prompt
+    let rustymq_url: String = Text::new("Enter RustyMQ URL?")
+    .with_default("rtmq://")
+    .prompt()
+    .unwrap();
+    println!("rustyMq URL: {}", rustymq_url);
+
+    // Example using a confirmation prompt
+    // let confirm = Confirm::new("Are you sure?")
+    // .with_default(false)
+    // .prompt()
+    // .unwrap();
+    // println!("Confirmation: {}", confirm);
+
+    // Example using a select prompt
+    // let options = vec!["Option 1", "Option 2", "Option 3"];
+    // let selected_option = Select::new("Select an option:", options)
+    // .prompt()
+    // .unwrap();
+    // println!("Selected: {}", selected_option);
 }
