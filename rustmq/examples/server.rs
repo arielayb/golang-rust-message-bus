60ce15d03cd5beff25ca864f0d148866c1cc194f
arielayb <cyberspace009@gmail.com>
2025-07-29 00:08:33 -0600

    trying to get traits to work with generics WIP

--- rustmq/examples/server.rs
+++ rustmq/examples/server.rs
@@ -1,17 +1,17 @@
-use inquire::{Confirm, Select, Text};
-use rustmq::rustmq_interface::DataPackage;
+use inquire::Text;
+use rustmq::prelude::tcp_stream_writer::TcpStreamWriter;
+use rustmq::rustmq_interface::RustMQ;
+use rustmq::tklog::{error, info};
+use rustmq::{Uuid, rustmq_interface::DataPackage};
 use std::collections::VecDeque;
 use std::io::{self, Read};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::thread;
-use rustmq::tklog::{info, error};
-use rustmq::{rustmq_interface, Uuid};
-use rustmq::prelude::{tcp_stream_writer::TcpStreamWriter};
 
 #[derive(Clone, Copy, Debug)]
 pub struct InitQueue;
 
-impl InitQueue {
+/*impl InitQueue {
     fn init_queue(self, stream: TcpStream, addr: SocketAddr) -> io::Result<Vec<u8>> {
         println!("RustMQ Stream created, {:?}", addr);
         let mut tcp_stream_writer = TcpStreamWriter::new(stream)?;
@@ -28,7 +28,7 @@ impl InitQueue {
 
         Ok(data_pkg.msg)
     }
-}
+}*/
 
 fn main() {
     // Example using a text prompt
@@ -43,12 +43,29 @@ fn main() {
         .parse()
         .expect("Cannot parse the url string! Did you include a port number?");
     let listener = TcpListener::bind(addr).unwrap();
+    println!("RustMQ Stream created, {:?}", addr);
+
+    //let mut tcp_stream_writer = TcpStreamWriter::new(stream)?;
+    //TODO: start setting up the queue creation and messages.
+    //let message: Vec<u8> = tcp_stream_writer.read_message()?;
+    /*let mut data_pkg = DataPackage {
+        msg: vec![],
+        uuid: Uuid::new_v4().to_string(),
+        msg_queue: VecDeque::new(),
+    };*/
 
     for stream in listener.incoming() {
+        let mut data_pkg = DataPackage {
+            msg: Box::new(Vec::new()),
+            uuid: Uuid::new_v4().to_string(),
+            msg_queue: VecDeque::new(),
+        };
+
         if let Ok(stream) = stream {
             thread::spawn(move || {
                 // init queue/tcp connection
-                let _q: Result<Vec<u8>, io::Error> = queue.init_queue(stream, addr);
+                //let _q: Result<Vec<u8>, io::Error> = queue.init_queue(stream, addr);
+                let _q: _ = data_pkg.rustmq_init_queue(addr, stream);
             })
             .join()
             .expect("recv msg thread completed.")
--- rustmq/src/rustmq_interface/mod.rs
+++ rustmq/src/rustmq_interface/mod.rs
@@ -1,28 +1,29 @@
 use crate::graph_system;
+use crate::tcp_stream_writer::TcpStreamWriter;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::io;
 use std::io::Bytes;
+use std::io::Read;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::vec::Vec;
-use tklog::{info, error};
-use crate::tcp_stream_writer::TcpStreamWriter;
+use tklog::{error, info};
 pub use uuid::Uuid;
 
-trait RustMQ {
+pub trait RustMQ {
     fn rustmq_init_queue(&mut self, addr: SocketAddr, stream: TcpStream) -> io::Result<()>;
-    fn rustmq_create_queue(&mut self) -> &VecDeque<u8>;
-    fn rustmq_get_queue(&self) -> &VecDeque<u8>;
-    fn rustmq_set_queue(&mut self, msg_queue: VecDeque<u8>);
+    fn rustmq_create_queue(&mut self) -> &VecDeque<Box<Vec<u8>>>;
+    fn rustmq_get_queue(&self) -> &VecDeque<Box<Vec<u8>>>;
+    fn rustmq_set_queue(&mut self, msg_queue: VecDeque<Box<Vec<u8>>>);
     fn rustmq_stream(&self);
-    fn rustmq_write(&mut self, message: Vec<u8>);
+    fn rustmq_write(&mut self, message: Box<Vec<u8>>);
     fn rustmq_read(&self);
 }
 
 pub struct DataPackage {
-    pub msg: Vec<u8>,
+    pub msg: Box<Vec<u8>>,
     pub uuid: String,
-    pub msg_queue: VecDeque<u8>,
+    pub msg_queue: VecDeque<Box<Vec<u8>>>,
 }
 
 impl RustMQ for DataPackage {
@@ -30,36 +31,36 @@ impl RustMQ for DataPackage {
         info!("RustMQ Stream created, ", addr);
         let mut tcp_stream_writer = TcpStreamWriter::new(stream)?;
         //TODO: start setting up the queue creation and messages.
-        self.msg = tcp_stream_writer.read_message()?;
+        //let msg = tcp_stream_writer.read_message()?;
         self.uuid = Uuid::new_v4().to_string();
-        
-        self.msg_queue = VecDeque::new();
-
-        // tcp_stream_writer.send_message(&message)?;
-        println!("the message from client: {:?}", self.msg);
+        let msg: Box<Vec<u8>> = Box::new(tcp_stream_writer.read_message().unwrap());
+        let msgstr: Vec<u8> = tcp_stream_writer.read_message().unwrap();
+        println!("the message from client: {:?}", &msg.to_vec());
+        self.msg_queue.push_back(msg);
+        println!("the message from client: {:?}", &self.msg_queue[0]);
         info!("message in queue");
 
         Ok(())
     }
 
-    fn rustmq_create_queue(&mut self) -> &VecDeque<u8> {
-        let data: VecDeque<u8> = VecDeque::new();
+    fn rustmq_create_queue(&mut self) -> &VecDeque<Box<Vec<u8>>> {
+        let data: VecDeque<Box<Vec<u8>>> = VecDeque::new();
         self.msg_queue = data;
 
         return &self.msg_queue;
     }
 
-    fn rustmq_get_queue(&self) -> &VecDeque<u8> {
+    fn rustmq_get_queue(&self) -> &VecDeque<Box<Vec<u8>>> {
         return &self.msg_queue;
     }
 
-    fn rustmq_set_queue(&mut self, msg_queue: VecDeque<u8>) {
+    fn rustmq_set_queue(&mut self, msg_queue: VecDeque<Box<Vec<u8>>>) {
         self.msg_queue = msg_queue;
     }
 
     fn rustmq_stream(&self) {}
 
-    fn rustmq_write(&mut self, message: Vec<u8>) {
+    fn rustmq_write(&mut self, message: Box<Vec<u8>>) {
         self.msg = message;
     }
 
